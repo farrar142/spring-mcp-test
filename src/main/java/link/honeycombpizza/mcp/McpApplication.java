b0a2b21bd6e6570556596125ac069eab172ef683
farrar142 <gksdjf1690@gmail.com>
2025-07-10 10:21:41 +0900

    mcp server 설정 완료

--- src/main/java/link/honeycombpizza/mcp/McpApplication.java
+++ src/main/java/link/honeycombpizza/mcp/McpApplication.java
@@ -1,7 +1,10 @@
 package link.honeycombpizza.mcp;
 
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class McpApplication {
@@ -10,4 +13,11 @@ public class McpApplication {
 		SpringApplication.run(McpApplication.class, args);
 	}
 
+	@Bean
+	public ToolCallbackProvider weatherTools(WeatherService weatherService){
+		return MethodToolCallbackProvider
+				.builder()
+				.toolObjects(weatherService)
+				.build();
+	}
 }
