e06fe661a7f5e7ed24f483d329b445f571b82825
farrar142 <gksdjf1690@gmail.com>
2025-07-10 09:53:58 +0900

    mcp client 설정

--- src/main/java/link/honeycombpizza/mcp/AiConfig.java
+++ src/main/java/link/honeycombpizza/mcp/AiConfig.java
@@ -1,19 +1,29 @@
 package link.honeycombpizza.mcp;
 
 
+import io.modelcontextprotocol.client.McpSyncClient;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class AiConfig {
+    @Autowired
+    private SyncMcpToolCallbackProvider toolCallbackProvider;
+
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
-        return chatClientBuilder.build();
+        return chatClientBuilder
+                .defaultToolCallbacks(toolCallbackProvider.getToolCallbacks())
+                .build();
     }
     @Bean
     public ChatMemoryRepository chatMemoryRepository() {
--- src/test/java/link/honeycombpizza/mcp/McpApplicationTests.java
+++ src/test/java/link/honeycombpizza/mcp/McpApplicationTests.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -12,11 +15,19 @@ class McpApplicationTests {
 	@Autowired
 	private ChatClient chatClient;
 
+
 	@Test
 	void testChatClient(){
 		String content = chatClient.prompt().user("2+2=?").call().content();
 		assertThat(content)
 				.contains("4");
+	}
+
+	@Test
+	void testMcpClient(){
+		String content = chatClient.prompt("네가 사용 가능한 tool 목록을 알려줘?").call().content();
+		assertThat(content)
+				.contains("spring_ai_mcp_client_filesystem_read_file");
 	}
 
 }
