3cf8d1159f2123bb4a06729d6bd2936146d429bd
farrar142 <gksdjf1690@gmail.com>
2025-07-14 12:44:28 +0900

    디렉터리 접근권한 테스트

--- src/test/java/link/honeycombpizza/mcp/McpApplicationTests.java
+++ src/test/java/link/honeycombpizza/mcp/McpApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class McpApplicationTests {
@@ -31,5 +32,16 @@ class McpApplicationTests {
 		assertThat(content)
 				.contains("spring_ai_mcp_client_filesystem_read_file");
 	}
+	@Test
+	void testDirectoryAccess(){
+		String content = chatClient.prompt("파일 시스템에서 '.' 디렉토리의 파일 목록을 알려줘?").call().content();
+		assertThat(content)
+				.contains("gradlew")
+				.doesNotContain("application.yml");
 
+		String content2 = chatClient.prompt("파일 시스템에서 '/Applications' 디렉토리의 파일 목록을 알려줘?")
+				.call()
+				.content();
+		System.out.println(content2);
+	}
 }
